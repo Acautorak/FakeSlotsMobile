1b378a4b4a0ac101d5052aa4f5607b533db9adc6
Utorak <acislav@gmail.com>
2025-06-09 21:06:28 +0200

    push2

---
+++ Assets/Scripts/Symbol.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Symbol : MonoBehaviour
+{
+    public enum SymbolType
+    {
+        Red = 1,
+        White = 2,
+        Green = 3,
+    }
+
+    public SymbolType type;
+
+
+}
